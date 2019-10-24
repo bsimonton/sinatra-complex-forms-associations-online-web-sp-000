271a210863bdd6f9a1fcd334966f6acc73caffff
Brian Simonton <bsimonton78@gmail.com>
2019-10-24 12:47:00 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,22 +5,48 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
 
+    @pet = Pet.create(params["pet"])
+
+    if !params["owner"]["name"].empty?
+      
+      @owner = Owner.create(name: params["owner"]["name"])
+      @owner.pets << @pet
+    end
+
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
+    @owners = Owner.all
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
 
-    redirect to "pets/#{@pet.id}"
-  end
+
+
+
+     patch '/pets/:id' do
+    ####### bug fix
+    if !params[:pet].keys.include?("owner_id")
+      params[:pet]["owner_id"] = []
+    end
+    #######
+
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @owner = Owner.create(name: params["owner"]["name"])
+      @owner.pets << @pet
+    end
+    redirect "pets/#{@pet.id}"
+  end 
 end
